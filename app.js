9fa6b3b1ed992ec18ab61f625ba7d5fe3a8de35d
topgirl2410 <maria.topgirl82@gmail.com>
2025-09-17 16:48:19 +0200

    galeria acabada

--- app.js
+++ app.js
@@ -9,4 +9,37 @@ menuOpenButton.addEventListener("click", () => {
 
 
 
-menuCloseButton.addEventListener("click", () => menuOpenButton.click())  
+menuCloseButton.addEventListener("click", () => menuOpenButton.click())
+
+const swiper = new Swiper('.slider-wrapper', {
+    // Optional parameters
+    loop: true,
+    grabCursor: true,
+    spaceBetween: 25,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        dynamicBullets: true,
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    // Responsive breakpoints
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        1024: {
+            slidesPerView: 3,
+        },
+    }
+});
